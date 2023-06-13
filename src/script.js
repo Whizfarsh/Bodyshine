6fb69df53d6055053f00c4bbd7e02ebfc44a8528
Whizfarsh <oyefarsh@gmail.com>
2023-06-13 01:15:08 +0100

    work on the review pannel slider

--- src/script.js
+++ src/script.js
@@ -40,6 +40,80 @@ about.forEach((abt) => {
 	abt.classList.add("revealfromdown");
 });
 
+//Review sliders
+const slides = document.querySelectorAll(".review");
+const rvBtnBack = document.querySelector(".review-btn-back");
+const rvBtnFront = document.querySelector(".review-btn-front");
+const dotCointainer = document.querySelector(".review-dots");
+
+let curSlide = 0;
+const numSlides = slides.length - 1;
+
+console.log(slides);
+
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+
+const createDots = function () {
+	slides.forEach(function (_, i) {
+		dotCointainer.insertAdjacentHTML(
+			"beforeend",
+			`<button class="dots__dot" data-slide="${i}"></button>`
+		);
+	});
+};
+createDots();
+
+const activeDots = function (slide) {
+	document
+		.querySelectorAll(".dots__dot")
+		.forEach((dot) => dot.classList.remove("dots__dot--active"));
+	document
+		.querySelector(`.dots__dot[data-slide="${slide}"]`)
+		.classList.add("dots__dot--active");
+};
+activeDots(0);
+
+const gotoSlide = function (slide) {
+	slides.forEach(
+		(s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+	);
+};
+gotoSlide(0);
+
+const nextSlide = function () {
+	if (curSlide === numSlides) {
+		curSlide = 0;
+	} else {
+		curSlide++;
+	}
+	gotoSlide(curSlide);
+	activeDots(curSlide);
+};
+const previousSlide = function () {
+	if (curSlide <= 0) {
+		curSlide = 0;
+	} else {
+		curSlide--;
+	}
+	gotoSlide(curSlide);
+	activeDots(curSlide);
+};
+
+rvBtnFront.addEventListener("click", nextSlide);
+rvBtnBack.addEventListener("click", previousSlide);
+
+document.addEventListener("keydown", function (e) {
+	e.key === "ArrowLeft" && previousSlide();
+	e.key === "ArrowRight" && nextSlide();
+});
+
+dotCointainer.addEventListener("click", function (e) {
+	if (e.target.classList.contains("dots__dot")) {
+		const { slide } = e.target.dataset;
+		gotoSlide(slide);
+		activeDots(slide);
+	}
+});
 //Mobile Navs
 const btnClose = document.querySelector(".mb-btnclose");
 const btnOpen = document.querySelector(".mb-btnopen");
